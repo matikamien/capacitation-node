2139eb74a9abaa3ef1c938e089752e1837dd4d19
Matias Kamien <matikamien_93@hotmail.com>
2016-12-01 11:56:32 -0300

    Add feature server returns hour

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var server = http.createServer();
+
+server.on("request", function(req, res) {
+	var date = new Date();
+	res.end("La hora es: " + date.getHours() + ":" + date.getMinutes());
+});
+
+server.listen(3000);
