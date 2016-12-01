fd7cc1a6fc6f6bcac55a15d8beef6eab8c0e0659
Matias Kamien <matikamien_93@hotmail.com>
2016-12-01 14:03:06 -0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var http = require("http");
+var url = require("url");
 
 var server = http.createServer();
 
 server.on("request", function(req, res) {
-	var date = new Date();
-	res.end("La hora actual es: " + date.getHours() + ":" + date.getMinutes());
+	//var date = new Date();
+	//res.end("La hora actual es: " + date.getHours() + ":" + date.getMinutes());
+
+	var urlData = url.parse(req.url, true);
+	res.end(JSON.stringify(urlData));
 });
 
 server.listen(process.env.PORT || 3000);
